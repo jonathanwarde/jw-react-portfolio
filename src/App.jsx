35d574b02eeb10c74cf956fb7f29d93ad75fbdbc
Jon Warde <jon.warde@7dots.co.uk>
2023-12-06 15:33:54 +0000

    css grid 'named areas' w/ minmax for responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
 import './scss/global.scss';
 import { createRoot } from "react-dom/client";
 import Project from "./Project";
+import Nav from "./Nav";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 const App = () => {
     return (
         <div>
-            <h1>Jonathan Warde</h1>
-            <p>Web developer</p>
-            <Project name="Top Secret Comedy Club" description="Marketing site and Vue.js events management app for UK's top rated comedy cub" />
+            <header>
+                <DarkModeSwitch />
+                <div className="content-grid">
+                    <Nav />
+                    <h1>Full stack (front end focussed) web developer</h1>
+                    <p>Jonathan Warde</p>
+                </div>
+            </header>
+            <div className="content-grid">
+                <main>
+                    <p><em>I make websites</em></p>
+                    <p>I design and build websites with a focus on standards, performance, and accessibility. With a decade of professional experience — in-house and remote — I've delivered for everyone. Whether it's PWAs, WordPress, or full-stack frameworks, I have the know-how to help.</p>
+                </main>
+                <aside>
+                    <div className="section">
+                        <div>
+                            <h2>Jonathan Warde</h2>
+                            <p></p>
+                            <ul>
+                                <li>
+                                    <a href='mailto:jonwarde@gmail.com'>
+                                        <span>email: </span>jonwarde@gmail.com</a>
+                                </li>
+                                <li>
+                                    <a href='https://linkedin.com/jonwarde'>
+                                        <span>linkedin: </span>linkedin/jonwarde</a>
+                                </li>
+                                <li>
+                                    <a href='https://github.com/jonathanwarde'>
+                                        <span>github: </span>github.com/jonathanwarde</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2>Projects</h2>
+                            <Project name="Spinnup" description="Multi-region marketing site for Universal Music Group distribution service" />
+                            <Project name="Top Secret Comedy Club" description="Website and Vue.js events management app for UK's top rated comedy cub" />
+                        </div>
+                    </div>
+                    <div className="section">
+                        <h2>Blog</h2>
+                        recent posts here
+                        categories here
+                    </div>
+                </aside>
+            </div>
         </div>
     );
 };
