7db0aa798dcc44a585cd1f056b676c67d9a25e19
Jon Warde <jon.warde@7dots.co.uk>
2023-12-07 12:21:58 +0000

    nav and links

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,14 @@
+import './scss/components/_nav.scss';
 const Nav = (props) => {
     return (
-        <nav>
-            <button>home</button>
-            <button>projects</button>
-            <button>services</button>
-            <button>blog</button>
-            <button>contact</button>
+        <nav className="main-nav">
+            <ul>
+                <li><a href="">home</a></li>
+                <li><a href="">projects</a></li>
+                <li><a href="">services</a></li>
+                <li><a href="">blog</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
         </nav>
     );
 };
