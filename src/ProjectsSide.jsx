79155735955c272a5fe3fb044330bb6c2cd4ddb5
Jon Warde <jon.warde@7dots.co.uk>
2023-12-09 18:40:07 +0000

    styling

--- src/ProjectsSide.jsx
+++ src/ProjectsSide.jsx
@@ -3,9 +3,9 @@ import Project from "./Project";
 const ProjectSide = (props) => {
     return (
         <div>
-            <h2>Projects</h2>
-            <Project name="Spinnup" description="Multi-region marketing site for Universal Music Group distribution service" img="./img/spinnup.png" />
-            <Project name="Top Secret Comedy Club" description="Website and Vue.js events management app for UK's top rated comedy cub" img="./img/tscc.png" />
+            <h2 className="h3">Projects</h2>
+            <Project name="Spinnup" description="Multi-region marketing site for Universal Music Group distribution service" img="./img/spinnup.png" link="https://spinnup.com" />
+            <Project name="Top Secret Comedy Club" description="Website and Vue.js events management app for UK's top rated comedy cub" img="./img/tscc.png" link="https://thetopsecretcomedyclub.co.uk" />
         </div>
     );
 };
