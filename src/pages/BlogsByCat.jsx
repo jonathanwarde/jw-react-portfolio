bb5d55da6b4ae77d2a339314a97b0cb83b8a78f0
Jon Warde <jon.warde@7dots.co.uk>
2023-12-21 14:12:30 +0000

    get post cats and post cat name
    - 2 x requests, Promise resolve all
    - update useState to store both values for use in render

--- src/pages/BlogsByCat.jsx
+++ src/pages/BlogsByCat.jsx
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
 const BlogsByCat = () => {
     const { id } = useParams();
-    const [posts, setPosts] = useState([]);
+    // Update the state to hold both posts and the category name
+    const [data, setData] = useState({ posts: [], categoryName: '' });
 
     useEffect(() => {
         if (id) {
-            fetch(`https://jonwarde.co.uk/wp/wp-json/wp/v2/posts?categories=${id}`)
-                .then(response => response.json())
-                .then(data => {
-                    setPosts(data);
+            const catNamesURL = `https://jonwarde.co.uk/wp/wp-json/wp/v2/categories/${id}`;
+            const catPostsURL = `https://jonwarde.co.uk/wp/wp-json/wp/v2/posts?categories=${id}`;
+
+            Promise.all([
+                fetch(catNamesURL).then(response => response.json()),
+                fetch(catPostsURL).then(response => response.json())
+            ])
+                .then(([catData, postsData]) => {
+                    // Set the state with both posts and the category name
+                    if (Array.isArray(postsData)) {
+                        setData({
+                            posts: postsData,
+                            categoryName: catData.name // Assuming 'name' is the field for the category name
+                        });
+                    } else {
+                        console.error('Expected an array of posts, but got:', postsData);
+                    }
                 })
                 .catch(error => {
                     console.error('Error fetching data: ', error);
                 });
         }
-    }, [id]); 
+    }, [id]);
 
     return (
         <div className="blog-posts-container">
+            {/* Render the category name */}
+            <h1>{data.categoryName}</h1>
             <ul>
-                {posts.map(post => (
+                {Array.isArray(data.posts) && data.posts.map(post => (
                     <li key={post.id} className="margin-bottom">
-                        <h3 className="h4">{post.title.rendered}</h3>
+                        <h2 className="h3">{post.title.rendered}</h2>
                         <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
                     </li>
                 ))}
