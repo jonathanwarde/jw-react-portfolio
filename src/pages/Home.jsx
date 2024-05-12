69f808cdc6192c98fa71a00b449c9f7352d8f019
Jon Warde <jon.warde@7dots.co.uk>
2024-05-12 07:02:09 +0100

    copy updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = (props) => {
             <p>My primary focus is on front-end development and user experience.</p>
             <p>I take pride in creating engaging, user-friendly websites, and I'm particularly skilled in building custom themes for WordPress, tailoring them to fit unique client needs.</p>
 
-            <p>I currently work at digital agency <a href="https://7dots.co.uk" target="_blank">7dots</a> - who's clients inclide Coca-Cola, Aston Martin, ITV, and BupaCoca-Cola, Aston Martin, ITV, and Bupahold a Senior Developer position at 7dots.co.uk, building sites in both Wordpress and Silverstripe. My commitment is towards crafting not only functional but also visually appealing and accessible web solutions, staying at the forefront of both established and emerging web technologies.</p>
+            <p>I currently work as a senior developer at digital agency <a href="https://7dots.co.uk" target="_blank">7dots</a> (who's clients include Coca-Cola, Aston Martin, ITV) building sites in both Wordpress and Silverstripe. My commitment is towards crafting not only functional but also visually appealing and accessible web solutions, staying at the forefront of both established and emerging web technologies.</p>
         </div>
     );
 };
