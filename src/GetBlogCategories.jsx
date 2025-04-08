7f444da2dcb7a36090ef3f119ba5185a1d7fb8c4
Jon Warde <jon.warde@7dots.co.uk>
2025-04-08 11:08:18 +0100

    loading spinner for when cats fetched from cms

--- src/GetBlogCategories.jsx
+++ src/GetBlogCategories.jsx
@@ -5,15 +5,14 @@ import {Link} from "react-router-dom";
 
 const GetBlogCategories = () => {
     const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // First fetch the posts
-        fetch('https://jonwarde.co.uk/wp/wp-json/wp/v2/posts')
+        setLoading(true)
+        fetch('https://jonwarde.co.uk/wp/wp-json/wp/v2/posts?per_page=60')
             .then(response => response.json())
             .then(posts => {
-                // Extract category IDs from posts
                 let categoryIds = [...new Set(posts.flatMap(post => post.categories))];
-
                 // remove category 1 (uncategorised)
                 categoryIds = categoryIds.filter(function(item) {
                     return item !== 1;
@@ -31,6 +30,7 @@ const GetBlogCategories = () => {
             .then(responses => Promise.all(responses.map(res => res.json())))
             .then(categoriesData => {
                 setCategories(categoriesData);
+                setLoading(false)
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -38,6 +38,14 @@ const GetBlogCategories = () => {
     }, []);
 
     return (
+        <>
+        {loading ? (
+            <svg version="1.1" class="loader" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0">
+            <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+              <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform>
+            </path>
+            </svg>
+          ) : (
         <div>
             <ul>
                 {categories.map(category => (
@@ -48,7 +56,8 @@ const GetBlogCategories = () => {
                     </li>
                 ))}
             </ul>
-        </div>
+        </div>)}
+        </>
     );
 };
 
